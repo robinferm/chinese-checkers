d303a2b9719f6f4567db8287d94bc0145ee682e3
Robin Ferm <robinferm@live.com>
2021-09-20 13:19:59 +0200

    GetRandomCharacter()

--- chinese-checkers.Core/Models/Player.cs
+++ chinese-checkers.Core/Models/Player.cs
@@ -13,6 +13,7 @@ namespace chinese_checkers.Core.Models
         public NestColor NestColor { get; set; }
         public int? Placement { get; set; }
 
+        // Player
         public Player(int id, ICharacter character, NestColor nestColor)
         {
             this.Id = id;
@@ -20,11 +21,35 @@ namespace chinese_checkers.Core.Models
             this.NestColor = nestColor;
         }
 
+        // AI
         public Player(int id, NestColor nestColor)
         {
             this.Id = id;
             this.IsAI = true;
             this.NestColor = nestColor;
+            this.Character = GetRandomCharacter();
+        }
+
+        private ICharacter GetRandomCharacter()
+        {
+            Random rnd = new Random();
+            switch (rnd.Next(5))
+            {
+                case 0:
+                    return new Mage();
+                case 1:
+                    return new Mage();
+                case 2:
+                    return new Mage();
+                case 3:
+                    return new Mage();
+                case 4:
+                    return new Mage();
+                case 5:
+                    return new Mage();
+                default:
+                    return null;
+            }
         }
     }
 }
